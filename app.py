e260ae82a519eb0c2fdc67096e57095f4eb63c1a
KesterDaniel <kesterdan17@gmail.com>
2023-01-21 15:28:11 +0100

    screen setup

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
 import random
 from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+
+
+
+
+screen.exitonclick()
