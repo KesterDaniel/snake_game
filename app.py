6c0b43f874baf1de88dfd5fc19d429b8cab2c9c5
KesterDaniel <kesterdan17@gmail.com>
2023-01-22 11:49:55 +0100

    created the snake body

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
-
-
-
+cor = 0
+for i in range(3):
+    new_turtle = Turtle(shape="square")
+    new_turtle.color("white")
+    new_turtle.setx(-cor)
+    cor += 20
 
 screen.exitonclick()
